cb8a60f01ab252288780efaf50080592f447f29b
walterarce <walterarce@outlook.com>
2024-06-21 23:41:10 -0300

    storage
    configuración inicial del storage

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCc4Z5XWUj56704bwMNDFALe46_Xs4fEIQ",
@@ -17,3 +18,5 @@ export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 
 export const auth = getAuth(app);
+
+export const storage = getStorage(app);
